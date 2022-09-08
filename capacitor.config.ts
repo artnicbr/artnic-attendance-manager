88b1869945d61e1167ab252dadafd07ad3cd4c04
Andre Ricardo Cintra <andre.cintra@unimedsaocarlos.local@1302-10866.unimedsaocarlos.local>
2022-09-08 15:18:40 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'artnic-attendance-manager',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
